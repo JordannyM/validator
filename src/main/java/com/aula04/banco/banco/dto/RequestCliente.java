2d7effbe758641efda8dab9c22cae94fca277ad7
JordannyM <jordanny.mizukoshi@gmail.com>
2022-03-07 21:47:31 -0300

    Testes

--- src/main/java/com/aula04/banco/banco/dto/RequestCliente.java
+++ src/main/java/com/aula04/banco/banco/dto/RequestCliente.java
@@ -2,6 +2,7 @@ package com.aula04.banco.banco.dto;
 
 import com.aula04.banco.banco.utils.CPF;
 import com.aula04.banco.banco.utils.Senha;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.validator.constraints.Length;
@@ -10,6 +11,7 @@ import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
 @Getter @Setter
+@AllArgsConstructor
 public class RequestCliente {
     @NotNull(message = "nao pode ser null vacilao") @NotEmpty(message = "batata") @Length(min = 2)
     private String nome;
@@ -19,4 +21,5 @@ public class RequestCliente {
     private String cpf;
     private String senha;
     private Integer agencia;
+
 }
